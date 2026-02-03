06d086bb04366188876cd268c8b6853d27efab49
Tobias Heimboeck <tobiasheimboeck@outlook.com>
2026-02-03 18:46:08 +0100

    feat: add Maven publishing configuration for GitHub Packages in database-common module

--- database-common/build.gradle.kts
+++ database-common/build.gradle.kts
@@ -23,3 +23,48 @@ tasks.named<Jar>("jar") {
     from(rootProject.file("LICENSE"))
     duplicatesStrategy = DuplicatesStrategy.INCLUDE
 }
+
+// Maven Publishing für GitHub Packages
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            
+            groupId = project.group.toString()
+            artifactId = "database-common"
+            version = project.version.toString()
+            
+            pom {
+                name.set("Database Common")
+                description.set("Default implementations for Database API")
+                url.set("https://github.com/${project.findProperty("github.owner")}/${project.findProperty("github.repo")}")
+                
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                
+                developers {
+                    developer {
+                        id.set("TGamings")
+                        name.set("Tobias Heimboeck")
+                        email.set("tobias.heimboeck@nexalit.at")
+                    }
+                }
+            }
+        }
+    }
+    
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/${project.findProperty("github.owner")}/${project.findProperty("github.repo")}")
+            credentials {
+                username = project.findProperty("github.username") as String? ?: System.getenv("GITHUB_ACTOR")
+                password = project.findProperty("github.token") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
