54827e9d942d14e86dcb6e25364285f45c964bfd
Tobias Heimboeck <tobiasheimboeck@outlook.com>
2026-02-08 23:49:20 +0100

    feat: introduce task scheduling system with ScheduledTask and TaskScheduler interfaces; implement DefaultTaskScheduler for task management; remove FormattingTestCommand

---
+++ database-api/src/main/java/dev/spacetivity/tobi/hylib/database/api/scheduler/ScheduledTask.java
@@ -0,0 +1,4 @@
+package dev.spacetivity.tobi.hylib.database.api.scheduler;
+
+public class ScheduledTask {
+}
---
+++ database-common/src/main/java/dev/spacetivity/tobi/hylib/database/common/api/scheduler/DefaultTaskScheduler.java
@@ -0,0 +1,4 @@
+package dev.spacetivity.tobi.hylib.database.common.api.scheduler;
+
+public class DefaultTaskScheduler {
+}
